6f8ed5e893026f0505595370a1724ff38353bed4
Ladkan <37418569+Ladkan@users.noreply.github.com>
2026-03-25 22:42:55 +0100

    Updates

---
+++ assets/ProtectedRoute-CaH9rHAY.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-D-oznMWL.js";import"./pocketbase-C8dUhLHD.js";import{_ as t,b as n,y as r}from"./index-yHVcCTHb.js";var i=e();function a(){let{isAuthenticated:e}=t();return e?(0,i.jsx)(n,{}):(0,i.jsx)(r,{to:`/kaban-board/login`,replace:!0})}export{a as default};
